f7438e612410135f7e4accba1e2c40883f8a0e93
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-12 13:14:59 -0700

    add delete, put, patch route tests and pass

---
+++ spec/requests/delete_route_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "delete an animal", :type => :request do
+
+  let!(:animal) { FactoryBot.create(:animal)}
+  before do
+    delete "/animals/#{animal.id}", params: { :age => 9 }
+  end
+
+  it 'returns delete message' do
+    expect(JSON.parse(response.body)['message']).to eq("This animal has been deleted successfully")
+  end
+
+  it 'returns a updated status' do
+    expect(response).to have_http_status(:ok)
+  end
+end
--- spec/requests/put_patch_route_spec.rb
+++ spec/requests/put_patch_route_spec.rb
@@ -15,3 +15,19 @@ describe "update an animal route", :type => :request do
     expect(response).to have_http_status(:ok)
   end
 end
+
+describe "update an animal route", :type => :request do
+
+  let!(:animal) { FactoryBot.create(:animal)}
+  before do
+    patch "/animals/#{animal.id}", params: { :age => 9 }
+  end
+
+  it 'returns the animal age' do
+    expect(JSON.parse(response.body)['message']).to eq("This animal has been updated successfully")
+  end
+
+  it 'returns a updated status' do
+    expect(response).to have_http_status(:ok)
+  end
+end
