a0b4c4df6ad97dee472072a827a876eb557eb93d
Chisato Murakami <kiwipom1031@gmail.com>
2020-07-20 11:16:47 -0700

    comment out rack_attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 class Rack::Attack
-  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
-  throttle('api/ip', limit: 300, period: 2.minutes) do |req|
-    req.ip
-  end
+  # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  # throttle('api/ip', limit: 300, period: 2.minutes) do |req|
+  #   req.ip
+  # end
 end
