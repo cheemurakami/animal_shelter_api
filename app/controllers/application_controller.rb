71ba4fd9f2478bd3e9b434d9cd7e31b4893d6e54
Chisato Murakami <kiwipom1031@gmail.com>
2020-07-22 12:46:50 -0700

    Undo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
   include Response
   rescue_from ActiveRecord::RecordNotFound do |exception|
     json_response({ message: exception.message }, :not_found)
